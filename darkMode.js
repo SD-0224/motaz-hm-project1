7631c874aa00e321d3c2faf7068009b77622697c
Motaz HM <motazhajm@gmail.com>
2024-02-18 03:04:38 +0200

    Update darkmode logic to store preferences

--- darkMode.js
+++ darkMode.js
@@ -1,15 +1,29 @@
 const darkToggleButton = document.getElementById("dark-toggle");
+const darkIconElement = document.getElementById("dark-icon");
+let isDarkMode = localStorage.getItem("theme") !== "light";
 
 detectDarkMode();
 darkToggleButton.addEventListener("click", darkToggle);
 
 //functions
 function darkToggle() {
+  isDarkMode = !isDarkMode;
+  localStorage.setItem("theme", isDarkMode ? "dark" : "light");
   document.body.classList.toggle("dark");
+  darkIconToggle();
 }
 
 function detectDarkMode() {
-  if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
-    darkToggle();
+  if (!isDarkMode) {
+    localStorage.setItem("theme", "light");
+    document.body.classList.toggle("dark");
+  }
+}
+
+function darkIconToggle() {
+  if (darkIconElement.getAttribute("name") === "sunny") {
+    darkIconElement.setAttribute("name", "moon-outline");
+  } else {
+    darkIconElement.setAttribute("name", "sunny");
   }
 }
