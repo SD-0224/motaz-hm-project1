f36694a979d1a5e0171b9109027d5af0844e84ec
Motaz HM <motazhajm@gmail.com>
2024-02-20 17:07:10 +0200

    Add favourites toggle logic

---
+++ favourites.js
@@ -0,0 +1,9 @@
+const favButton = document.getElementById("favourites-toggle")
+const favSection = document.getElementById("favrourites-section")
+
+favButton.addEventListener("click", favourtiesVisibilityToggle)
+
+function favourtiesVisibilityToggle() {
+    favSection.classList.toggle("push-to-bottom")
+}
+
