0728a515b9bb6c1b7434fd8f0fd7890ae5dfca7b
Motaz HM <motazhajm@gmail.com>
2024-03-06 22:17:49 +0200

    Add debounced search logic

--- scripts/index/index.js
+++ scripts/index/index.js
@@ -1,16 +1,23 @@
 import { fetchData } from "../modules/fetch.js";
 import { displayList, addFilterTypes } from "./indexView.js";
-const dataPath = "https://tap-web-1.herokuapp.com/topics/list";
+import { debounce } from "../modules/debounce.js";
 
+const dataPath = "https://tap-web-1.herokuapp.com/topics/list";
 const data = await fetchData(dataPath);
 
 displayList(data);
 addFilterTypes(data);
 
+const searchElem = document.getElementById("filter-search");
 const sortElem = document.getElementById("sortby");
 const filterElem = document.getElementById("filterby");
+
 let sortType = "";
 let filterType = "";
+
+searchElem.addEventListener("input", (event) => {
+  debouncedSearchData(event.target.value);
+});
 
 sortElem.addEventListener("change", (event) => {
   let sortedData = [];
@@ -29,6 +36,13 @@ filterElem.addEventListener("change", (event) => {
 });
 
 // functions
+const debouncedSearchData = debounce(searchData, 300);
+
+async function searchData(searchValue) {
+  const searchData = await fetchData(`https://tap-web-1.herokuapp.com/topics/list?phrase=${searchValue}`);
+  displayList(searchData);
+}
+
 function sortData(data, sortType) {
   if (!sortType) {
     return data;
---
+++ scripts/modules/debounce.js
@@ -0,0 +1,11 @@
+export function debounce(callback, delay) {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
