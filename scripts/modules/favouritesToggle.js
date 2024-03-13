db4156eb0a55dd6b434ac9a8aa0920d71135ba8a
Motaz HM <motazhajm@gmail.com>
2024-03-13 21:12:13 +0200

    Convert to react (first commit)

--- scripts/modules/favouritesToggle.js
+++
@@ -1,8 +0,0 @@
-const favButton = document.getElementById("favourites-toggle");
-const favSection = document.getElementById("favrourites-section");
-
-favButton.addEventListener("click", favourtiesVisibilityToggle);
-
-export function favourtiesVisibilityToggle() {
-  favSection.classList.toggle("push-to-bottom");
-}
--- scripts/modules/fetch.js
+++
@@ -1,8 +0,0 @@
-export async function fetchData(path) {
-  try {
-    const response = await fetch(path);
-    return await response.json();
-  } catch (error) {
-    return;
-  }
-}
