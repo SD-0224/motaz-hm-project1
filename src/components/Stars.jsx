f59fd18e7189a07c1b51ea3d1626678e1031a7af
Motaz HM <motazhajm@gmail.com>
2024-03-19 13:32:47 +0200

    Refactor Stars code

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 
 const Stars = ({ percentage }) => {
+  const starsOutline = makeStarsArray(false);
+  const starsFilled = makeStarsArray(true);
+
   return (
     <div className="stars-outer-container">
-      <ion-icon size="medium" name="star-outline"></ion-icon>
-      <ion-icon size="medium" name="star-outline"></ion-icon>
-      <ion-icon size="medium" name="star-outline"></ion-icon>
-      <ion-icon size="medium" name="star-outline"></ion-icon>
-      <ion-icon size="medium" name="star-outline"></ion-icon>
-
+      {starsOutline}
       <div
         className="stars-inner-container"
         style={{ clipPath: `polygon(0 0, ${percentage}% 0, ${percentage}% 100%, 0% 100%)` }}
       >
-        <ion-icon size="medium" name="star"></ion-icon>
-        <ion-icon size="medium" name="star"></ion-icon>
-        <ion-icon size="medium" name="star"></ion-icon>
-        <ion-icon size="medium" name="star"></ion-icon>
-        <ion-icon size="medium" name="star"></ion-icon>
+        {starsFilled}
       </div>
     </div>
   );
 };
+
+function makeStarsArray(isFilled) {
+  let stars = [];
+  for (let i = 0; i < 5; i++) {
+    stars.push(<ion-icon size="medium" name={isFilled ? "star" : "star-outline"}></ion-icon>);
+  }
+  return stars;
+}
 
 export default Stars;
